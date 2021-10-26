7bb515d3c6e791a8db6e73b5106fe92b1236389c
barthflo <flrnt.barth@gmail.com>
2021-10-26 14:26:13 +0200

    removed unused Redirect

--- client/src/views/Admin/HomeView/index.jsx
+++ client/src/views/Admin/HomeView/index.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import { Redirect } from 'react-router-dom';
 import styled from 'styled-components';
 import Page from '../../../components/Page';
 import Section from '../../../components/Section';
